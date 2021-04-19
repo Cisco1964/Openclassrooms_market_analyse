bad3c3bb8f1502d83114734360a5c11d39b5abc5
francis <francis.cazeres@gmail.com>
2021-04-19 18:30:14 +0200

    Ajout choix d'une catégorie

--- categorie.py
+++ categorie.py
@@ -14,10 +14,8 @@ def livre(url, soup):
     for book in all_books:
         # url de chaque livre
         h3 = book.h3.find("a")["href"]
-        #print(h3)
         # url absolue
         link = urljoin(urlpage, h3)
-        #print(link)
         # Appel url du livre
         page = urllib.request.urlopen(link)
         # chargement html dans variable soup
@@ -33,14 +31,12 @@ def livre(url, soup):
         # image
         image_url = book.a.img["src"]
         image_url = urljoin(url, image_url)
-        # print(image_url)
         # Récupération categorie
         ul = soup_book.find("ul", class_ = "breadcrumb")
         category = ul.find_all("a")[2].text     
         # Récupération autres informations
         table_data = soup_book.find("table", class_ = "table table-striped")
         td = table_data.findAll("td")
-        #print(td)
         # écriture du livre 
         rows.append([link, td[0].text, title, td[2].text, td[3].text, td[5].text, product_description, category, td[6].text, image_url])
         # attente avant prochaine appel
@@ -58,8 +54,9 @@ def une_categorie(urlpage):
     if soup.find("li", class_ = "next"):
         time.sleep(3)
         li = soup.find("li", class_ = "next")
-        url = urljoin("http://books.toscrape.com/catalogue/category/books/sequential-art_5/",li.find("a").get('href'))
-        print(url)
+        # mise en forme url
+        pos = urlpage.rfind("/")
+        url = urlpage[:pos+1] + li.find("a").get('href')
         # Appel de la fonction si autre page
         une_categorie(url)
     
@@ -68,16 +65,38 @@ rows = []
 rows.append(['product_page_url', 'universal_product_code (upc)', 'title', 'price_including_tax',
              'price_excluding_tax', 'number_available','product_description', 'category', 'review_rating', 'image_url'])
 
-# chargement url
-urlpage =  'http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html' 
-une_categorie(urlpage)
 
-# Lecture rows pour écriture
-for i in rows:
+liste = ("travel","mistery","historical fiction","sequential art","Classics","philosophy","romance",
+ "womens fiction","fiction","childrens","religion", "nonfiction", "Music", "default",
+ "science fiction","sports and Games","add a comment","fantasy", "new adult", "young adult", 
+ "science", "poetry","paranormal", "art", "psychology", "autobiography", "parenting", "adult fiction",
+ "humor", "horror", "history", "food and drink", "christian fiction", "business", "biography",
+ "thriller", "contemporary", "spirituality", "academic", "self help", "historical", "christian",
+ "suspense", "short stories", "novels", "health", "politics", "cultural", "erotica", "crime")
+
+print('Entrez une catégorie :')
+categ = input()
+categ = categ.lower()
+if categ == " ":
+    print("catégorie obligatoire")
+elif categ not in liste:
+    print("cette catégorie n'existe pas")
+else:
+    i = liste.index(categ)
+    cat = categ.replace(" ","-")
+    cat = cat + "_" + (str(i+2))
+
+    # chargement url
+    urlpage =  'http://books.toscrape.com/catalogue/category/books/{}/index.html'.format(cat) 
+    une_categorie(urlpage)
+
+    # Lecture rows pour écriture
+    for i in rows:
     # creation csv
-    with open('category_sequential-art.csv','w', newline='') as f_output:
-        csv_output = csv.writer(f_output)
-        csv_output.writerows(rows)
+        nom_fichier = '{}.csv'.format(cat)
+        with open(nom_fichier,'w', newline='') as f_output:
+            csv_output = csv.writer(f_output)
+            csv_output.writerows(rows)
 
 
 
